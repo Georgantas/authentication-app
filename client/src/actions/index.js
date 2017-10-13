d55cbdeeb2bbdd3511346f9f364e305cbc71ee1b
Georgantas <john.georan@icloud.com>
2017-10-12 22:44:07 -0400

    progress

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -33,3 +33,8 @@ export function signinUser({email, password}){
             });
     }
 }
+
+export function signoutUser(){
+    localStorage.removeItem('token');
+    return { type: UNAUTH_USER };
+}
